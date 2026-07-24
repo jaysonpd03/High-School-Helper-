ac0e5bad19b61982798c219b907465612c8e0994
Jayson Purl <jaysonpurl@MacBookAir.lan>
2026-07-23 21:15:24 -0400

    Added a tool to reliably evaluate exponents

--- app/tools/math_tools.py
+++ app/tools/math_tools.py
@@ -1,6 +1,14 @@
 import math
 from pydantic_ai import RunContext
 
+# Debug function
+def debug():
+	"""
+	Function to print debug statment to screen to insure agent is using tools
+	"""
+	return print("Tool called!")
+
+
 def square_root(value: float) -> float:
 	"""
 	 Calculate the square root of a non-negative number
@@ -12,7 +20,7 @@ def square_root(value: float) -> float:
 	 	The square root of value
 	"""
 	# Debug line to see if agent actually uses tool
-	# print("Tool called!")
+	debug()
 
 	if value < 0:
 		raise ValueError("Square root is only defined for non-negative numbers.")
@@ -34,7 +42,7 @@ def quadratic_solver(a: float, b: float, c: float):
 	"""
 
 	# Debug line
-	print("Tool called!")
+	debug()
 
 	# Check to see if equation is quadratic
 	if a==0:
@@ -67,5 +75,36 @@ def quadratic_solver(a: float, b: float, c: float):
 					"root2": root,
 					"nature": "One repeated real solution"
 				}
+def calculate_exponent(base: float, exponent: float) -> float:
+	"""
+	Function to compute exponentiation
+
+	Use this tool whenever a user asks to:
+	- evaluate expressions like 2^3
+	- calculate powers
+	- compute exponents
+	- raise one number to another power
+
+	Examples:
+		2^3
+		10 to the power of 4
+		7 raised by -2
+		16**2
+		(-3) to the second power
+		36^0.5
+	
+	Arguments:
+		base: base number or number being raised
+		exponent: the exponent or power
+
+	Returns:
+		Result of base raised to the exponent
+	"""
+
+	# Debug
+	debug()
+
+	return base ** exponent
+
 
 
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from app.agent import agent
 
-result = agent.run_sync("Solve the following problems: 1. x^2 - 4x + 4=0 2. x^2 + 2x + 5=0 3. 2x^2 + 7x - 4=0 4. 5x + 3=0")
+result = agent.run_sync("Use the calculate_exponent tool to evaluate 17^23")
 
 print(result.output)
